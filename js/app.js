b9406828e58b698fa367426887bb553908fcf588
stevenremenapp <remenapp@gmail.com>
2018-07-18 23:10:03 -0400

    Add player to screen; add player movement; add enemies to screen; add single kind of enemy movement

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'img/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * 10;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,10 +28,47 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor() {
+        this.sprite = 'img/char-cat-girl.png';
+        this.x = 203;
+        this.y = 380;
+    }
+
+    update() {
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(direction) {
+        if (direction === 'up') {
+            this.y -= 83;
+        }
+        if (direction === 'down') {
+            this.y += 83;
+        }
+        if (direction === 'right') {
+            this.x += 101;
+        }
+        if (direction === 'left') {
+            this.x -= 101;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+let player = new Player();
+
+let allEnemies = [];
+for (let i = 0; i <= 6; i++) {
+    allEnemies.push(new Enemy(10 * i, 30 * i));
+}
 
 
 
